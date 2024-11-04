02ca74e0e178727a2198c7f88350d108dca57ef4
Sam Ly <sam@samly.me>
2024-11-03 23:08:56 -0800

    implement types

---
+++ src/synth/Types.ts
@@ -0,0 +1,25 @@
+export type WaveForm = "sine" | "square" | "triangle" | "sawtooth";
+
+export type UnisonConfig = {
+    enabled: boolean,
+    waveFor?: WaveForm,
+    detune?: number,
+    gain?: number,
+}
+
+export type ADSREnvelope = {
+    attack: number,
+    decay: number,
+    sustain: number,
+    release: number,
+}
+
+export type SynthConfig = {
+    waveForm: WaveForm,
+
+    unisons: UnisonConfig[],
+
+    volumeEnvelope: ADSREnvelope,
+
+    filterEnvelope: ADSREnvelope,
+}
