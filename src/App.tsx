b33da7fc5364cc812eb5a4157da359ddba07e089
Sweetzzzz <jacob.lembach@gmail.com>
2024-11-05 00:38:57 -0800

    added first collumn of frontend

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,64 @@ import { useState } from "react"
 import Synth from "./synth/Synth"
 import InteractivePiano from "./components/InteractivePiano";
 
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import Slider from '@mui/material/Slider';
+import * as React from 'react'
+
+import "./index.css"
+
+
+
+
 function App() {
 
   const [ready, setReady] = useState<boolean>(false);
 
   return (
     <>
+
+      <div className=" ml-10 border w-[30vw] border-black">
+        <ul>
+          <li className=" p-2">VOICE</li>
+          <Slider defaultValue={0.3} max={1} min={0} step={0.1} sx={{ width: 200, marginLeft: 2, height: 30, color: "#d9d9d9", borderRadius: 0, '& .MuiSlider-thumb': { opacity: 0 } }} />
+          <li className=" flex items-center">
+            <span className=" pl-2 mr-2">DET</span>
+            <Slider defaultValue={30} sx={{ width: 200, height: 30, color: "#d9d9d9", borderRadius: 0, '& .MuiSlider-thumb': { opacity: 0 } }} />
+          </li>
+          <li><hr className="border-1 border-black pb-12"></hr></li>
+          <li><hr className="border-1 border-black"></hr></li>
+          <li className=" p-2">UNISON 1</li>
+          <Slider defaultValue={30} sx={{ width: 200, marginLeft: 2, height: 30, color: "#d9d9d9", borderRadius: 0, '& .MuiSlider-thumb': { opacity: 0 } }} />
+          <li className=" flex items-center">
+            <span className=" pl-2 mr-2">DET</span>
+            <Slider defaultValue={30} sx={{ width: 200, height: 30, color: "#d9d9d9", borderRadius: 0, '& .MuiSlider-thumb': { opacity: 0 } }} />
+          </li>
+          <li><hr className="border-1 border-black pb-12"></hr></li>
+          <li><hr className="border-1 border-black"></hr></li>
+          <li className=" p-2">UNISON 2</li>
+          <Slider defaultValue={30} sx={{ width: 200, marginLeft: 2, height: 30, color: "#d9d9d9", borderRadius: 0, '& .MuiSlider-thumb': { opacity: 0 } }} />
+          <li className=" flex items-center">
+            <span className=" pl-2 mr-2">DET</span>
+            <Slider defaultValue={30} sx={{ width: 200, height: 30, color: "#d9d9d9", borderRadius: 0, '& .MuiSlider-thumb': { opacity: 0 } }} />
+          </li>
+        </ul>
+      </div>
+
+
+
       {
-        ready ? <InteractivePiano/> :
+        ready ? <InteractivePiano /> :
           <button onClick={() => { new Synth(); setReady(true); }}>
             start
           </button>
       }
+
+
     </>
   )
 }
--- src/components/InteractivePiano.tsx
+++ src/components/InteractivePiano.tsx
@@ -4,7 +4,7 @@ import Synth from '../synth/Synth';
 
 export default function InteractivePiano() {
     const firstNote = MidiNumbers.fromNote('c3');
-    const lastNote = MidiNumbers.fromNote('f5');
+    const lastNote = MidiNumbers.fromNote('f4');
     const keyboardShortcuts = KeyboardShortcuts.create({
         firstNote: firstNote,
         lastNote: lastNote,
