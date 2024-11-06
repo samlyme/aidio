27f944d9585bcd2d2b4cba6edc8983a283d1cb83
Sweetzzzz <jacob.lembach@gmail.com>
2024-11-06 02:48:10 -0800

    idk anymore man its 3am

--- src/App.tsx
+++ src/App.tsx
@@ -34,125 +34,12 @@ function App() {
         ready ?
           ( 
             <>
-            <div className=" flex gap-5 mt-5 mr-5 pb-10"> 
-            <div className=" ml-5 border w-[45vw] border-black">
-              <ul>
-                <li className=" p-2">VOICE</li>
-                <CustomSlider />
-                <li className=" flex items-center">
-                  <span className=" pl-2 mr-2">DET</span>
-                  <CustomSlider />
-                </li>
-                <li><hr className="border-1 border-black pb-12"></hr></li>
-                <li><hr className="border-1 border-black"></hr></li>
-                <li className=" p-2">UNISON 1</li>
-                <CustomSlider />
-                <li className=" flex items-center">
-                  <span className=" pl-2 mr-2">DET</span>
-                  <CustomSlider />
-                </li>
-                <li><hr className="border-1 border-black pb-12"></hr></li>
-                <li><hr className="border-1 border-black"></hr></li>
-                <li className=" p-2">UNISON 2</li>
-                <CustomSlider />
-                <li className=" flex items-center">
-                  <span className=" pl-2 mr-2">DET</span>
-                  <CustomSlider />
-                </li>
-              </ul>
-            </div>
+         
 
-
-               
-            <div className="  border w-[45vw] border-black">
-              <ul>
-                <li className=" p-2">ASDR - VOLUME</li>
-                <li className=" flex items-center">
-                  <span className=" pl-2 mr-2">ATK</span>
-                  <CustomSlider />
-                </li>
-                <li className=" flex items-center">
-                  <span className=" pl-2 mr-2">DEC</span>
-                  <CustomSlider />
-                </li>
-                <li className=" flex items-center">
-                  <span className=" pl-2 mr-2">SUS</span>
-                  <CustomSlider />
-                </li>
-                <li className=" flex items-center">
-                  <span className=" pl-2 mr-2">REL</span>
-                  <CustomSlider />
-                </li>
-                <li><hr className="border-1 border-black pb-12"></hr></li>
-                <li><hr className="border-1 border-black"></hr></li>
-                <li className=" p-2">ASDR - FILTER</li>
-                <li className=" flex items-center">
-                  <span className=" pl-2 mr-2">ATK</span>
-                  <CustomSlider />
-                </li>
-                <li className=" flex items-center">
-                  <span className=" pl-2 mr-2">DEC</span>
-                  <CustomSlider />
-                </li>
-                <li className=" flex items-center">
-                  <span className=" pl-2 mr-2">SUS</span>
-                  <CustomSlider />
-                </li>
-                <li className=" flex items-center">
-                  <span className=" pl-2 mr-2">REL</span>
-                  <CustomSlider />
-                </li>
-              </ul>
-            </div>
-
-
-            <div className="  border w-[45vw] border-black">
-              <ul>
-                <li className=" p-2">ASDR - VOLUME</li>
-                <li className=" flex items-center">
-                  <span className=" pl-2 mr-2">ATK</span>
-                  <CustomSlider />
-                </li>
-                <li className=" flex items-center">
-                  <span className=" pl-2 mr-2">DEC</span>
-                  <CustomSlider />
-                </li>
-                <li><hr className="border-1 border-black pb-12"></hr></li>
-                <li><hr className="border-1 border-black"></hr></li>
-                <li className=" p-2">ASDR - FILTER</li>
-                <li className=" flex items-center">
-                  <span className=" pl-2 mr-2">ATK</span>
-                  <CustomSlider />
-                </li>
-                <li className=" flex items-center">
-                  <span className=" pl-2 mr-2">DEC</span>
-                  <CustomSlider />
-                </li>
-                <li><hr className="border-1 border-black pb-12"></hr></li>
-                <li><hr className="border-1 border-black"></hr></li>
-              </ul>
-            </div>
-
-            <div className="  border w-[45vw] border-black">
-              <ul className=" h-full">
-                <li className="h-full ">
-                <textarea
-                placeholder="SEND A MESSAGE"
-                className="resize-none p-10 h-full w-full flex items-end pl-2 pb-0 text-wrap"
-                onFocus={() => setIsTextBoxFocused(true)}
-                onBlur={() => setIsTextBoxFocused(false)} 
-              ></textarea>
-                </li>
-              </ul>
-            </div>
-
-
-            </div>
-            
-            <InteractivePiano isTextBoxFocused={isTextBoxFocused} />
-           
             <SettingsMenu />
             <InteractivePiano isTextBoxFocused={isTextBoxFocused} />
+           
+           
             </>
             
           )
