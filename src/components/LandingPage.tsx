19ef14b68bb68589560c61ea8bcce4e58ea4538a
Sam Ly <sam@samly.me>
2024-11-06 19:04:08 -0800

    working

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,6 +1,5 @@
-const LandingPage = ({
-
-}) => {return (
+const LandingPage = () => {
+    return (
     <>
     <div className="flex  justify-center  w-full font-[universe] text-6xl mt-3">
     <h1>AIDIO</h1>
